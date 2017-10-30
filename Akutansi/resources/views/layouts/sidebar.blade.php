24e4d3731e4b8a76c606e9930fd6fecfca74a783
TaufanFadhilah <tfi_23@ymail.com>
2017-10-30 15:45:59 +0700

    Installed template

---
+++ Akutansi/resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,30 @@
+<aside class="side-navigation-wrap sidebar-fixed">  <!-- START: Side Navigation -->
+    <div class="sidenav-inner">
+
+        <ul class="side-nav magic-nav">
+
+            <li class="side-nav-header">Navigation</li>
+
+            <li>
+                <a href="index.html"><i class="sli-dashboard"></i> <span class="nav-text">Dashboard</span></a>
+            </li>
+
+            <li class="has-submenu">
+                <a href="#submenuOne" data-toggle="collapse" aria-expanded="false">
+                    <i class="fs-grid-6"></i>
+                    <span class="nav-text">Tables</span>
+                </a>
+                <div class="sub-menu collapse secondary" id="submenuOne">
+                    <ul>
+                        <li><a href="table-basic.html">Basic Tables</a></li>
+                        <li><a href="table-dataTable.html">Data Table</a></li>
+                        <li><a href="table-editable.html">Editable Table</a></li>
+                    </ul>
+                </div>
+            </li>
+
+        </ul>
+
+    </div><!-- END: sidebar-inner -->
+
+</aside>    <!-- END: Side Navigation -->
